e6dcbc43fb95fed4b2b4ae362bdfa64e758d7c9e
Troy <tborgesmail@yahoo.com>
2022-07-23 13:22:55 -0400

    Setting up a custom layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <div class="text-red-600 text-2xl">APP PAGE</div>
-    <div class="h-40 w-40 bg-gray-700">YOOO</div>
-    <!-- <NuxtWelcome /> -->
-  </div>
+  <NuxtLayout name="custom">
+    <LazyCounter />
+  </NuxtLayout>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
 // According to this directive, it is advised to import Tailwind inside the App.vue file
 import "@/assets/css/tailwind.css";
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  layout: false,
+});
 </script>

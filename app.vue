52c55968785331054af130d0eed04cd45c513566
Troy <tborgesmail@yahoo.com>
2022-07-23 10:33:21 -0400

    Setup Tailwind CSS
    According to this directive, it is advised to import Tailwind inside the App.vue file

--- app.vue
+++ app.vue
@@ -3,3 +3,8 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script lang="ts" setup>
+// According to this directive, it is advised to import Tailwind inside the App.vue file
+import "./assets/css/tailwind.css";
+</script>
